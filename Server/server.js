96f1c95b370dd3cf6c2d7d3b7b10ea29f5276432
Awais Butt <howlingvirgo08@gmail.com>
2026-09-10 19:15:38 +0500

    Completed project

--- Server/server.js
+++ Server/server.js
@@ -21,7 +21,7 @@ const port =  process.env.PORT || 4000;
 await connectDB();
 await connectCloudinary();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://grocer-app-theta.vercel.app']
 app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks)
 // MIDDLEWARE CONFIG
 app.use(express.json());
